e760f64481b4871bdb75751fd6f77f78d286425c
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-12-04 22:10:07 +0300

    revert mana charge back

--- config/skills.js
+++ config/skills.js
@@ -2676,7 +2676,7 @@ module.exports = {
 				type: 'charging',
 				length: [800, 1600],
 				noInterrupt: [26, 27, 38],
-				autoRelease: 0,
+				//autoRelease: 0,
 				glyphs: {
 					28031: { chargeSpeed: 0.25 }
 				}
